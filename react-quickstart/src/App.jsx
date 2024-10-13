6af9b8d94fe4684c281159bd6d369e7cfd5a5477
ChristianEliseoNavales <cenavalesisip@gmail.com>
2024-10-13 23:25:02 +0800

    I'm done completing the section on rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css'
-import AdminPanel from  './AdminPanel';
-import LoginForm from  './LoginForm';
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-export default function MyApp() {
-  let content;
-  let isLoggedIn = true;
-
-  if (isLoggedIn) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginForm />;
-  }
-
-  return (
-    <div>
-      {content}
-    </div>
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
   );
 
+  return (
+    <ul>{listItems}</ul>
+  );
 }
+
 
