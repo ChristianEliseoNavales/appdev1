59ce606e58f0b3fe0aaf808a640cf72cd0c35b7a
ChristianEliseoNavales <cenavalesisip@gmail.com>
2024-10-13 22:44:46 +0800

    Correcting the commit on the section on adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,11 +1,4 @@
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
+import './App.css'
 
 export default function MyApp() {
   return (
