2c4ec24485592dc2caf7cfed23bbc7495a51abc3
ChristianEliseoNavales <cenavalesisip@gmail.com>
2024-11-16 13:33:33 +0800

    Updated Counter Component, Done with ToggleButton Component

--- react-hooks/src/Components/Counter.jsx
+++ react-hooks/src/Components/Counter.jsx
@@ -2,26 +2,14 @@ import React, { useState } from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0);
-
-    const plusCount = () => {
-        setCount(count + 1)
-    };
-    const minusCount = () => {
-        if(count > 0){
-            setCount(count - 1)
-        } 
-        else {
-            alert("You've reached 0!")
-        }
-    };
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={plusCount}> Add </button>
-      <button onClick={minusCount}> Subtract </button>
-    </div>
-  )
-}
+  
+    return (
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+    );
+  };
 
 export default Counter
